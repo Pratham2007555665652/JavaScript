92f231d848ce26a74ad2cba858bb178cbbfabde3
Pratham <prathamshiroya7@gmail.com>
2026-07-25 16:21:20 +0530

    Create a file 01_variables.js in 01_Basics

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144329
+let accountEmail = "example23@gmail.com";
+var accountPassword = "12345";
+accountCity = "Surat";
+
+//undefined
+
+let accountState;
+
+// accountId = 2
+// not allowed 
+
+accountEmail = "hc@gmail.com";
+accountPassword = "1278977";
+accountCity = "bengaluru";
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState]);
