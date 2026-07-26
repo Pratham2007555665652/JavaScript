bbaf2b5f2e86012464438701f808a0c40a04384b
Pratham <prathamshiroya7@gmail.com>
2026-07-26 18:19:59 +0530

    02_Primitive_Datatypes

---
+++ 01_Basics/02_Primitive_Datatypes.js
@@ -0,0 +1,27 @@
+//These all are Primitive Datatypes
+
+//string Datatype
+let name = "roank";
+
+//BigInt Datatype
+let larger_Number = BigInt("1464564556456454645");
+
+//number Datatype
+let age = 17;
+
+//boolean Datatype
+let isLoggedIn = false;
+
+//undefined when not a single value is assign
+let state;
+
+//Null Datatype
+let control = null;
+
+//symbol Datatype
+let sym1 = Symbol("UI");
+
+//to check what is datatype of variable
+let data_type = typeof(isLoggedIn);
+
+console.log("Type of the isLoggedIn is",data_type);
