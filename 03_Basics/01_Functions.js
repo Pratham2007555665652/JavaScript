55bc8b0137a266375f48c2452880a901a304c606
Pratham <prathamshiroya7@gmail.com>
2026-08-08 17:15:15 +0530

    Refactor 01_Functions.js to enhance function examples and add new functionality

--- 03_Basics/01_Functions.js
+++ 03_Basics/01_Functions.js
@@ -34,3 +34,37 @@ function loginUserMessage(username = "Sam"){
 //if not any pass argument so it shows undefined
 let ms = loginUserMessage("mohan");
 console.log(ms);
+
+function calculateCartPrice(va1, val2,...n1){
+    return n1;
+}
+
+console.log(calculateCartPrice(200, 400, 500, 6500));
+
+const user = {
+    username: "mohan",
+    prices: 190
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);   
+
+}
+
+handleObject(user);
+
+//object as parameter here
+handleObject({
+    username: "sam",
+    price: 690
+})
+
+
+const myArray = [200, 400, 2300]
+
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+
+console.log(returnSecondValue(myArray));
+console.log(returnSecondValue([200, 300, 2030]));
