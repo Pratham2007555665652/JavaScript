8617b1bbddbbcf3b497d17c0f5c82cc2420499a6
Pratham <prathamshiroya7@gmail.com>
2026-08-10 18:28:05 +0530

    Refactor 02_scopes.js to improve code readability and organization

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -1,11 +1,45 @@
 let a = 200;
 
 if (true) {
-    let a = 10;
-    const b = 20;
-    console.log("Inner", a);
-    
+  let a = 10;
+  const b = 20;
+  //   console.log("Inner", a);
+}
+// console.log(a);
+// console.log(b);
+
+function one() {
+  const userName = "mohan";
+  function two() {
+    const website = "youtube";
+    console.log(userName);
+  }
+  // console.log(website);
+
+  // two();
 }
 
-console.log(a);
-// console.log(b);
+// one();
+
+if (true) {
+  const userName = "mohan";
+  if (userName === "mohan") {
+    const website = " instagram";
+    // console.log(userName + website);
+  }
+}
+
+//++++++++++++++++++++++++++++ Important ++++++++++++++++++++++++++++
+
+console.log(addOne(7));
+
+function addOne(num) {
+  return num + 1;
+}
+
+// ReferenceError: Cannot access 'addTwo' before initialization
+// addTwo(8);
+
+const addTwo = function (num) {
+  return num + 2;
+};
