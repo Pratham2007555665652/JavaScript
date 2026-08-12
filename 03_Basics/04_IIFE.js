b79e53f9416a832f028317e2a3243d2e2658756d
Pratham <prathamshiroya7@gmail.com>
2026-08-12 12:30:23 +0530

    Add 04_IIFE.js with examples of Immediately Invoked Function Expressions

---
+++ 03_Basics/04_IIFE.js
@@ -0,0 +1,12 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+(function chai() {
+  //named IIFE
+  console.log("DB CONNECTED");
+})();
+//add ; to end a function context
+
+((name) => {
+  //unnamed IIFE
+  console.log(`DB CONNECTED TWO ${name}`);
+})("mohan");
