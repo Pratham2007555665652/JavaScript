65bbf5e4cae73c8d97ad75d3add5cd3e340a43cf
Pratham <prathamshiroya7@gmail.com>
2026-08-13 13:22:13 +0530

    Add control flow examples in one.js, switch.js, and truthy.js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,57 @@
+// <, >, <=, >=, ==, !=, ===, !==
+
+//if
+// const isUserloggedIn = true;
+// const temprature = 41
+
+// if (temprature < 50){
+//     console.log("less than 50");
+    
+// }
+
+// else{
+//     console.log("temprature is grater than 50");
+    
+// }
+
+
+const score = 200;
+
+if (score > 100) {
+    let power = "fly"
+    console.log(`user power: ${power}`);
+}
+
+// const balance = 1000;
+
+// if ( balance < 500 ) {
+//     console.log("less than");
+// }
+// else if(balance< 750){
+//     console.log("less than 750");
+    
+// }
+// else if(balance< 900){
+//     console.log("less than 900");
+// }
+// else{
+//     console.log("less than 1200");
+    
+// }
+
+//  && => and || => or
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmai = true 
+
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+
+if(loggedInFromGoogle || loggedInFromEmai){
+    console.log("user logged in");  
+}
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march";
+
+switch (month) {
+    case "January":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("Feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("Default Case Match");
+        break;
+}
