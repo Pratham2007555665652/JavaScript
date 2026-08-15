69e3452a931da424a3d9bb9a0ef24f760ce3cb2e
Pratham <prathamshiroya7@gmail.com>
2026-08-15 10:46:27 +0530

    Add while and do-while loop examples in 02_while_do-while_loop.js

---
+++ 05_iterations/02_while_do-while_loop.js
@@ -0,0 +1,21 @@
+// let index = 0; 
+// while (index<=10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2; 
+// }
+
+let myarray = ["flash", "batman", "superman"];
+
+let arr = 0
+while (arr<myarray.length) {
+    console.log(`value is ${myarray[arr]}`);
+    // arr = arr + 1; //or
+    arr++;
+}
+
+let score = 1;
+
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score <= 11);
