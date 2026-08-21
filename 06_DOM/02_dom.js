52ff872aa83337842e53ff26928a3e5f24e5869c
Pratham <prathamshiroya7@gmail.com>
2026-08-21 22:07:17 +0530

    Add initial HTML structure and JavaScript for DOM manipulation

---
+++ 06_DOM/02_dom.js
@@ -0,0 +1,25 @@
+const parent = document.querySelector('.parent');
+console.log(parent);
+
+// console.log(parent.children);
+console.log(parent.children[1].innerHTML);
+
+for (let i = 0; i < parent.children.length; i++) {
+    // console.log(parent.children[i].innerHTML);
+    
+}
+
+parent.children[1].style.color = 'orange'
+
+// console.log(parent.firstElementChild);
+// console.log(parent.lastElementChild);
+
+
+const dayONne = document.querySelector('.day');
+// console.log(dayONne);
+
+// console.log(dayONne.parentElement);
+// console.log(dayONne.nextElementSibling);
+
+console.log("NODES: ", parent.childNodes);
+
